da3079db985c5b92bb51bdfeeb38644774d37418
prathiksatish9902 <119522408+prathiksatish9902@users.noreply.github.com>
2024-10-17 20:54:43 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 #include <math.h>
 int main()
 {
-    long int n , d1 , d2 ,d3 , d4 , d5 , revno ;
+    long int num , n1 , n2 ,n3 , n4 , n5 , revno ;
     printf("enter the 5 digit number :");
-    scanf("%d \n", &n);
-    d5 = n % 10;
-    n = n/10;
-    d4 = n % 10 ;
-    n = n/10 ;
-    d3 = n % 10 ;
-    n = n/10;
-    d2 = n % 10;
-    n = n/10;
-    d1 = n % 10;
-    n = n/10 ;
-    revno = d5 * 10000 + d4 * 1000 + d3 * 100 + d2 *10 + d1;
+    scanf("%d \n", &num);
+    n5 = num % 10;
+    num = num/10;
+    n4 = num % 10 ;
+    num = num/10 ;
+    n3 = num % 10 ;
+    num = num/10;
+    n2 = num % 10;
+    num = num/10;
+    n1 = num % 10;
+    num = num/10 ;
+    revno = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 *10 + n1;
     printf("the reversed number is : %d\n", revno);
 
     return 0;
